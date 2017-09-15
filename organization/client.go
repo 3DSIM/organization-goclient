f3e731f0ee78546d1a67b1041a1a7a352a0d45ee
Alex Drinkwater <adrinkwater@3dsim.com>
2017-09-15 09:56:55 -0600

    Add method for updating subscription

--- organization/client.go
+++ organization/client.go
@@ -43,6 +43,7 @@ type Client interface {
 	Organizations() ([]*models.Organization, error)
 	Organization(organizationID int32) (*models.Organization, error)
 	Subscriptions() ([]*models.Subscription, error)
+	UpdateSubscription(subscription *models.Subscription) (a *models.Subscription, err error)
 }
 
 type client struct {
@@ -140,7 +141,7 @@ func (c *client) Organization(organizationID int32) (org *models.Organization, e
 	return response.Payload, nil
 }
 
-func (c *client) Subscriptions() (orgList []*models.Subscription, err error) {
+func (c *client) Subscriptions() (subscriptionList []*models.Subscription, err error) {
 	defer func() {
 		// Until this issue is resolved: https://github.com/go-swagger/go-swagger/issues/1021, we need to recover from
 		// panics.
@@ -154,6 +155,26 @@ func (c *client) Subscriptions() (orgList []*models.Subscription, err error) {
 	}
 	params := operations.NewGetSubscriptionsParams()
 	response, err := c.client.Operations.GetSubscriptions(params, openapiclient.BearerToken(token))
+	if err != nil {
+		return nil, err
+	}
+	return response.Payload, nil
+}
+
+func (c *client) UpdateSubscription(subscription *models.Subscription) (a *models.Subscription, err error) {
+	defer func() {
+		// Until this issue is resolved: https://github.com/go-swagger/go-swagger/issues/1021, we need to recover from
+		// panics.
+		if r := recover(); r != nil {
+			err = fmt.Errorf("Recovered from panic: %v", r)
+		}
+	}()
+	token, err := c.tokenFetcher.Token(c.audience)
+	if err != nil {
+		return nil, err
+	}
+	params := operations.NewPutSubscriptionParams().WithOrgID(subscription.OrganizationID).WithSubID(subscription.ID).WithSubscription(subscription)
+	response, err := c.client.Operations.PutSubscription(params, openapiclient.BearerToken(token))
 	if err != nil {
 		return nil, err
 	}
